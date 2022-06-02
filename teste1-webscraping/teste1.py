06dc3d24b22e7d345f380dbb7a4f965f45ccb06a
Joao Santos <joaovictor.sale@gmail.com>
2022-06-02 12:29:12 -0300

    adição dos testes 1 e 2

---
+++ teste1-webscraping/teste1.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+import zipfile
+import os
+
+url = 'https://www.gov.br/ans/pt-br/assuntos/consumidor/o-que-o-seu-plano-de-saude-deve-cobrir-1/o-que-e-o-rol-de-procedimentos-e-evento-em-saude'
+response = requests.get(url)
+content = BeautifulSoup(response.text, 'html.parser')
+all_urls = content.find_all('a')
+
+def download():
+    for url in all_urls:
+        try:
+            if 'Anexo' in url['href']:
+            #print(url['href'])
+                file_url = url['href']
+
+                file_name = file_url.split('/')[-1]
+
+            #print(file_name)
+            
+                file_response = requests.get(file_url)
+
+                with open ('./teste1-webscraping/anexos/' + file_name, 'wb') as f:
+
+                    f.write(file_response.content)
+
+
+        except Exception as e:
+            print(e)
+
+download()
+
+#shutil.make_archive('anexos-zip', 'zip', './zipados/')
+#shutil.move('./anexos-zip.zip', './teste1-webscraping')
+
+def zip_directory(folder_path, zip_path):
+    with zipfile.ZipFile(zip_path, mode='w') as zipf:
+        len_dir_path = len(folder_path)
+        for root, _, files in os.walk(folder_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                zipf.write(file_path, file_path[len_dir_path:])
+                
+zip_directory('./teste1-webscraping/anexos/', './teste1-webscraping/anexos-zip.zip')
+
+print('its working!')
---
+++ teste2/teste2-transformacao-dados.py
@@ -0,0 +1,33 @@
+import tabula
+from zipfile import ZipFile
+
+pages = '3-200'
+pdf_path = 'https://www.gov.br/ans/pt-br/arquivos/assuntos/consumidor/o-que-seu-plano-deve-cobrir/Anexo_I_Rol_2021RN_465.2021_RN473_RN478_RN480_RN513_RN536.pdf'
+
+def extractPDF(pdf_path, pages):    
+    dfs = tabula.read_pdf(pdf_path, pages=pages)
+    i = 0
+    for df in dfs:       
+        if ('PROCEDIMENTO' in df.iloc[0].to_string()) and i == 0:            
+            
+            # df = df.rename(columns={'OD':'Seg. Odontológica'}) 
+            # df = df.rename(columns={'AMB':'Seg. Ambulatorial'})
+            # df['OD'] = df['OD'].replace('OD','Seg. Odontológica')
+            # df['Seg. Ambulatorial'] = df['Seg. Ambulatorial'].replace('AMB','Seg. Ambulatorial')            
+            
+            df.to_csv('teste2/' + 'Rol de Procedimentos e Eventos em Saúde' + '.csv ', sep=';', encoding='utf-8', index=False, header=True)
+            i = i + 1
+
+        else:
+            # df['OD'] = df['OD'].replace('OD','Seg. Odontológica')
+            # df['AMB'] = df['AMB'].replace('AMB','Seg. Ambulatorial')
+            
+            df.to_csv('teste2/' + 'Rol de Procedimentos e Eventos em Saúde' + '.csv ', sep=';', mode='a', encoding='utf-8', index=False, header=False)
+    zipCsv()
+
+
+def zipCsv():
+    with ZipFile('teste2/Teste_{Joao_Victor}.zip', 'w') as zipObj: #Cria um arquivo .zip
+        zipObj.write('teste2/' + 'Rol de Procedimentos e Eventos em Saúde' + '.csv')
+
+extractPDF(pdf_path, pages)
